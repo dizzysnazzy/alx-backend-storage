d479755a2a0f3ddaa8e15c79dce9147eb150f0f7
dizzysnazzy <dismaskipchumba2@gmail.com>
2022-04-27 12:21:55 -0700

    web file

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python3
-"""
-create a web cach
-"""
+"""Defines a method that implements an expiring web cache and tracker."""
+
+from typing import Callable
+from functools import wraps
 import redis
 import requests
-rc = redis.Redis()
-count = 0
 
 
+def requests_counter(method: Callable) -> Callable:
+    """ Counts how many times a requesthas been made
+    """
+    r = redis.Redis()
+
+    @wraps(method)
+    def wrapper(url):
+        """ wrapper fxn that counts actual no of requests made"""
+        r.incr(f"count:{url}")
+        cached_ = r.get(f"cached:{url}")
+        if cached:
+            return cached.decode('utf-8')
+
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
+@requests_counter
 def get_page(url: str) -> str:
-    """ get a page and cach value"""
-    rc.set(f"cached:{url}", count)
+    """ obtains html content for a given site url and returns it..
+    """
     resp = requests.get(url)
-    rc.incr(f"count:{url}")
-    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
     return resp.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
